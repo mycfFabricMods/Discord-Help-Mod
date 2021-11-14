fc7886c72241f1becbcb078cae803c6e154e39dc
rlDruDo <ichundeli@gmail.com>
2021-11-15 00:22:49 +0100

    mixin intro with inject Item.class

---
+++ src/main/java/me/mycf/discord_example/mixins/ItemMixin.java
@@ -0,0 +1,23 @@
+package me.mycf.discord_example.mixins;
+
+import net.minecraft.item.Item;
+import net.minecraft.item.Items;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(Item.class)
+public abstract class ItemMixin {
+
+    @Shadow @Final private boolean fireproof;
+
+    @Shadow public abstract Item asItem();
+
+    @Inject(method = "isFireproof", at = @At("HEAD"), cancellable = true)
+    private void ironIngotsDontBurn(CallbackInfoReturnable<Boolean> cir){
+        cir.setReturnValue(this.fireproof || this.asItem().equals(Items.IRON_INGOT));
+    }
+}
